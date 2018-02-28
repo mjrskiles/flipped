b4b55185a0c2e19fa197c9fe31379beeb5893f71
Michael Skiles <mjrskiles@gmail.com>
2018-02-27 23:48:57 -0600

    Initial commit

--- Flipped/LevelSelectViewController.swift
+++ Flipped/LevelSelectViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class LevelSelectViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
